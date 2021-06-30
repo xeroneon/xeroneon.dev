bf27830ccd248ab66f0e7630aa7c3967f2481321
Xeroneon <andrewvasquez0113@gmail.com>
2021-06-29 19:19:17 -0700

    renamed css files and got background working

---
+++ components/InfoPanel/InfoPanel.tsx
@@ -0,0 +1,11 @@
+import styles from './InfoPanel.module.scss';
+
+export default function InfoPanel() {
+
+    return (
+        <div className={styles.root}>
+            <div>something</div>
+            <div></div>
+        </div>
+    );
+}
