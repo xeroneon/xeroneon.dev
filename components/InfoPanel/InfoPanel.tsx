5dc9460d6420203570cbd567cfce8efeecdd8ebe
Xeroneon <andrewvasquez0113@gmail.com>
2021-06-29 19:51:32 -0700

    base css for info panel

--- components/InfoPanel/InfoPanel.tsx
+++ components/InfoPanel/InfoPanel.tsx
@@ -1,11 +1,18 @@
 import styles from './InfoPanel.module.scss';
+import Image from 'next/image';
 
 export default function InfoPanel() {
 
     return (
         <div className={styles.root}>
-            <div>something</div>
-            <div></div>
+            <div className={styles.name}>
+                <img src="/assets/images/me.jpg" className={styles.avatar} />
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <p style={{fontSize: '36px', margin: '0'}}>@Xeroneon</p>
+                    <p style={{fontSize: '18px', margin: '0'}}>Andrew Vasquez</p>
+                </div>
+            </div>
+            <div className={styles.links}></div>
         </div>
     );
 }
